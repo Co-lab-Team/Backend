5d859d2cb93e01b5fceaa65e0e2f5b3eb3ca0428
highb33kay <hiibeekayvibe@gmail.com>
2024-01-25 00:40:35 +0100

    Add templateURL field and multer package

--- src/controllers/temp.controler.ts
+++ src/controllers/temp.controler.ts
@@ -11,6 +11,8 @@ import { ResponseHandler } from "../utils/responsehandler";
 import { slugifyTemplate } from "../services/slugify";
 import { emailService } from "../services/mailer";
 
+import { uploadTemplate } from "../services/imageupload";
+
 export const createTemplate: RequestHandler = async (
   req: Request,
   res: Response,
@@ -20,14 +22,37 @@ export const createTemplate: RequestHandler = async (
     const { title, description, fileSize, template, category, tags } = req.body;
     const { userID } = req.params;
 
+    const file = req.file as any;
+    const { service } = req.body;
+
+    if (!file) {
+      throw new BadRequestError("Please Upload a Template");
+    }
+
+    // verify the user id
+    const validUser = await prisma.user.findUnique({
+      where: { userID: userID },
+      select: {
+        userID: true,
+      },
+    });
+
+    if (!validUser) {
+      throw new NotFoundError("User not found");
+    }
+
+    // todo: if file size is beyond 100M
+
+    const { url, size } = await uploadTemplate(file, service);
+
     const slug = slugifyTemplate(title);
 
     const newTemplate = await prisma.template.create({
       data: {
         title,
         description,
-        fileSize,
-        template,
+        fileSize: size,
+        templateURL: url,
         category,
         tags,
         slug,
--- src/routes/temp.route.ts
+++ src/routes/temp.route.ts
@@ -1,7 +1,34 @@
-import { Router } from "express";
+import { Router, NextFunction, Request, Response } from "express";
+import multer from "multer";
+import {
+  createTemplate,
+  uploadTemplateFile,
+} from "../controllers/temp.controler";
 
-import { createTemplate } from "../controllers/temp.controler";
+import { ForbiddenError } from "../middlewares";
 
 const router = Router();
 
 import { validationMiddleware } from "../middlewares/validationMiddleware";
+import { templateSchema } from "../middlewares/validations/temp.zod";
+
+const storage = multer.memoryStorage();
+const uploads = multer({ storage: storage }).single("file");
+
+const uploadHandler = (req: Request, res: Response, next: NextFunction) => {
+  uploads(req, res, function (err) {
+    if (err) {
+      const newForbbidenError = new ForbiddenError("You must upload one image");
+      next(newForbbidenError);
+    }
+    next();
+  });
+};
+
+// create template
+router.post(
+  "/template/",
+  uploadHandler,
+  validationMiddleware(templateSchema),
+  createTemplate
+);
